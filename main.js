dd1a5487a666c367b295cbab90ea270d2c05db64
sulaimon kadelu <sulaimonkadelu89@gmail.com>
2020-07-16 11:50:43 -0400

    projec-done

---
+++ main.js
@@ -0,0 +1,66 @@
+class Traveler{
+    constructor(name){
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+
+    }
+    hunt(){
+        this.food += 2
+
+    }
+    eat(){
+        if(this.food <= 0){
+            return this.isHealthy = false
+ 
+        }
+        this.food -= 1
+        
+    }
+
+}
+
+
+class Wagon{
+    constructor(capacity){
+        this.passengers = []
+        this.capacity = capacity
+
+    }
+    getAvailableSeatCount(){
+        return this.capacity - this.passengers.length
+
+
+    }
+
+    join(traveler){
+        if(this.getAvailableSeatCount()>= 1){
+            this.passengers.push(traveler)
+        }
+
+    }
+
+    shouldQuarantine(){
+        for(let index=0;index < this.passengers.length; index++){
+            if(this.passengers[index].isHealthy === false){
+                return true
+
+            }
+        }
+
+    }
+    
+    totalFood(){
+        let totalNumber = 0
+        for(let index = 0; index < this.passengers.length; index ++){
+            totalNumber += this.passengers[index].food
+
+        }
+            return totalNumber
+    }
+
+
+
+
+
+}
